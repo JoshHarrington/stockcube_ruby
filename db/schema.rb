6d671f75b06c9d91dbcf80932e828ddd6a0d374c
Josh Harrington <josh@joshharrington.co.uk>
2018-02-17 15:46:46 +0000

    advanced login implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217134610) do
+ActiveRecord::Schema.define(version: 20180217151708) do
 
   create_table "cupboards", force: :cascade do |t|
     t.string "location"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180217134610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
