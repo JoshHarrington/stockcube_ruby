17b0b84046972238038c2f2b1210455af8dea15d
Josh Harrington <josh@joshharrington.co.uk>
2018-02-17 15:16:36 +0000

    add header with user log in log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'sessions/new'
 
   get '/meals' => 'meals#index'
   get '/meals/new' => 'meals#new'
@@ -28,6 +31,9 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new', as: :users_new
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 end
