1832630df10f2c7e8cd27cb1b6caf84314fb00ac
Josh Harrington <josh@joshharrington.co.uk>
2018-02-20 11:00:57 +0000

    move recipe description def to recipe helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,6 @@
 module RecipesHelper
+	def recipe_description
+		description = @recipe.description
+		description.gsub!(/\n/, '<br />').html_safe
+	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,3 @@ class Recipe < ApplicationRecord
 	has_many :ingredients, through: :portions
 end
 
-def description_unescape
-	description = @recipe.description
-	description.gsub!(/\n/, '<br />').html_safe
-end
