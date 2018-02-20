5f5f3957df489f62cec14aeaa287363a2315e492
Josh Harrington <josh@joshharrington.co.uk>
2018-02-20 14:51:00 +0000

    add mailer to send password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
   get 'sessions/new'
@@ -37,4 +41,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
