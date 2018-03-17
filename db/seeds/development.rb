31b57aa67d1dc810bc8f7d96f6f5c2dabb25e5b4
Josh Harrington <josh@joshharrington.co.uk>
2018-03-17 20:26:41 +0000

    match production and development seed files

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -5,55 +5,71 @@ require 'uri'
 require 'date'
 
 
-## units
-puts "Creating Units and adding to table"
+unless Unit.exists?(unit_number: 3)
+	## units
+	puts "Creating Units and adding to table"
 
-## load units create from partial
-require('./db/seeds/partials/create_units.rb')
+	## load units create from partial
+	require('./db/seeds/partials/create_units.rb')
 
-puts "Units tables complete\n\n"
-
-## recipes
-puts "Starting recipes logic to add ingredients and recipes to tables"
-
-## include recipe global variables
-require('./db/seeds/partials/_recipe_global_vars.rb')
-
-## load create recipes and ingredients from partial
-require('./db/seeds/partials/create_recipes_and_ingredients.rb')
-
-puts "Recipes and relevant portions created\n\n"
+	puts "Units tables complete\n\n"
+end
 
 
-puts "Update ingredients attributes eg. whether gluten free or veggie"
+unless Recipe.exists?(id: 3) && Ingredient.exists?(id: 3)
+	## recipes
+	puts "Starting recipes logic to add ingredients and recipes to tables"
 
-## load update ingredient attributes from partial
-require('./db/seeds/partials/update_ingredient_attributes.rb')
+	if not Rails.env.downcase == "production"
+		## include recipe global variables
+		require('./db/seeds/partials/_recipe_global_vars.rb')
+	end
 
-puts "Finished updating ingredient attributes\n\n"
+	## load create recipes and ingredients from partial
+	require('./db/seeds/partials/create_recipes_and_ingredients.rb')
 
-
-puts "Create admin and standard users, create cupboards, add stock to cupboards"
-
-## load add stock to cupboards from partial
-require('./db/seeds/partials/create_users_cupboards_add_stock.rb')
-
-puts "Stock added to cupboards\n\n"
-
-
-puts "Creating fake test users"
-
-## load dev only create users from partial
-require('./db/seeds/partials/dev_create_test_users.rb')
-
-puts "Test users created\n\n"
+	puts "Recipes and relevant portions created\n\n"
 
 
-puts "Creating shopping lists"
+	puts "Update ingredients attributes eg. whether gluten free or veggie"
 
-## load create shopping lists from partial
-require('./db/seeds/partials/create_shopping_lists.rb')
+	## load update ingredient attributes from partial
+	require('./db/seeds/partials/update_ingredient_attributes.rb')
 
-puts "Shopping lists created\n\n"
+	puts "Finished updating ingredient attributes\n\n"
+end
+
+unless User.exists?(admin: true) && Cupboard.exists?(id: 1)
+
+	puts "Create admin and standard users, create cupboards, add stock to cupboards"
+
+	## load add stock to cupboards from partial
+	require('./db/seeds/partials/create_users_cupboards_add_stock.rb')
+
+	puts "Stock added to cupboards\n\n"
+
+end
+
+unless User.exists?(admin: false)
+	if not Rails.env.downcase == "production"
+
+		puts "Creating fake test users"
+
+		## load dev only create users from partial
+		require('./db/seeds/partials/dev_create_test_users.rb')
+
+		puts "Test users created\n\n"
+
+	end
+end
+
+unless ShoppingList.exists?(id: 1)
+	puts "Creating shopping lists"
+
+	## load create shopping lists from partial
+	require('./db/seeds/partials/create_shopping_lists.rb')
+
+	puts "Shopping lists created\n\n"
+end
 
 puts "FINISHED!\n\n"
