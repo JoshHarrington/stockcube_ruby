938bdb88f5332050c9ef8cbbd78795e6051daed5
Josh Harrington <josh@joshharrington.co.uk>
2018-03-17 17:41:12 +0000

    update production seed file

--- db/seeds/partials/create_recipes_and_ingredients.rb
+++ db/seeds/partials/create_recipes_and_ingredients.rb
@@ -1,3 +1,16 @@
+if Rails.env.downcase == "production"
+  veggieRecipesXML = File.read("./db/foodDBs/VegetarianRecipes.exl")
+  worldRecipesXML = File.read("./db/foodDBs/WorldRecipes.exl")
+
+  veggieRecipes = Nokogiri::XML(veggieRecipesXML)
+  worldRecipes = Nokogiri::XML(worldRecipesXML).search('data')
+
+  RecipeVars.recipes = veggieRecipes.at('data').add_child(worldRecipes)
+
+  RecipeVars.foodRegex = "(, boiled.*)|(, blanched.*)|(, california.*)|(, pan fried.*)|(, braised.*)|(, roasted.*)|(, chuck clod.*)|(, fresh.*)|(, salad or.*)|(, instant.*)|(, prepared.*)|(, sliced.*)|(, canned.*)|(, salted.*)|(, dry roasted.*)|(, plain.*)|(, double acting.*)|(, Eagle.*)|(, from .*)|(, degerminated.*)|(, ground.*)|(, all purpose.*)|(, pastry.*)|(, white.*)|(, cooked.*)|(, grated.*)|(, shredded.*)|(, organic.*)|(, dash.*)|(, whole grain.*)|(, chopped.*)|(, extracted.*)|(, unsweetened.*)|(, frozen.*)|(, red, California.*)|(, old fashioned.*)|(, dry.*)|(, original.*)|(, TSP.*)|(, seedless.*)|(, without seeds.*)|(, table.*)|(, brown.*)|(, with calcium.*)|(, winter.*)|(, powdered.*)|(, silken.*)|(, with nigari.*)|(, unsalted.*)|(, crushed.*)|(, stewed.*)|(, filtered.*)|(, natural.*)|(, municipal.*)|(, regular.*)|(, baked.*)|(, active.*)|(, steamed.*)|(, ready to.*)|(, diced.*)|(, powder.*)|(, defatted.*)|(, toasted.*)|(, hulled.*)|(, oriental.*)|(, daikon.*)|(, halves.*)|(, creamy.*)|(, flakes.*)|(, vital.*)|(, slivered.*)|(, 60 grain.*)|(, raw.*)|(, top round.*)|(, top sirloin.*)|(, chuck.*)|(, dehydrated.*)|(, seasoned.*)|(, low moisture.*)|(, roasted.*)|(, whole.*)|(, kernels.*)|(, 50 grain.*)|(, refrigerated.*)|(, smoked.*)|(, food service.*)|(, elegant.*)|(, chilled.*)"
+  RecipeVars.recipeRegex = "(Copyright.*)|(www\..*\n.*\n.*)"
+end
+
 recipes = RecipeVars.recipes
 foodRegex = RecipeVars.foodRegex
 recipeRegex = RecipeVars.recipeRegex
--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -4,20 +4,27 @@ require 'set'
 require 'uri'
 require 'date'
 
+load(Rails.root.join( 'db', 'seeds', 'partials', "create_units.rb"))
 
 ## units
 puts "Creating Units and adding to table"
 
 ## load units create from partial
-require('./db/seeds/partials/create_units.rb')
+# require('./db/seeds/partials/create_units.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "create_units.rb"))
 
 puts "Units tables complete\n\n"
 
 ## recipes
 puts "Starting recipes logic to add ingredients and recipes to tables"
 
+## include recipe global variables
+# require('./db/seeds/partials/_recipe_global_vars.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "_recipe_global_vars.rb"))
+
 ## load create recipes and ingredients from partial
-require('./db/seeds/partials/create_recipes_and_ingredients.rb')
+# require('./db/seeds/partials/create_recipes_and_ingredients.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "create_recipes_and_ingredients.rb"))
 
 puts "Recipes and relevant portions created\n\n"
 
@@ -25,7 +32,8 @@ puts "Recipes and relevant portions created\n\n"
 puts "Update ingredients attributes eg. whether gluten free or veggie"
 
 ## load update ingredient attributes from partial
-require('./db/seeds/partials/update_ingredient_attributes.rb')
+# require('./db/seeds/partials/update_ingredient_attributes.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "update_ingredient_attributes.rb"))
 
 puts "Finished updating ingredient attributes\n\n"
 
@@ -33,6 +41,18 @@ puts "Finished updating ingredient attributes\n\n"
 puts "Create admin and standard users, create cupboards, add stock to cupboards"
 
 ## load add stock to cupboards from partial
-require('./db/seeds/partials/create_users_cupboards_add_stock.rb')
+# require('./db/seeds/partials/create_users_cupboards_add_stock.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "create_users_cupboards_add_stock.rb"))
 
-puts "Stock added to cupboards"
+puts "Stock added to cupboards\n\n"
+
+
+puts "Creating shopping lists"
+
+## load create shopping lists from partial
+# require('./db/seeds/partials/create_shopping_lists.rb')
+load(Rails.root.join( 'db', 'seeds', 'partials', "create_shopping_lists.rb"))
+
+puts "Shopping lists created\n\n"
+
+puts "FINISHED!\n\n"
