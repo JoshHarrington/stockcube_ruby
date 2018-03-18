aee49094233ce0998f00fd6d792b93896380750e
Josh Harrington <josh@joshharrington.co.uk>
2018-03-18 19:54:11 +0000

    IN PROGRESS: working to add editable stock in cupboard fields

--- app/models/cupboard.rb
+++ app/models/cupboard.rb
@@ -1,5 +1,11 @@
 class Cupboard < ApplicationRecord
 	has_many :stocks
 	has_many :ingredients, through: :stocks
+
 	belongs_to :user
+
+	accepts_nested_attributes_for :stocks,
+	:reject_if => :all_blank,
+	:allow_destroy => true
+	accepts_nested_attributes_for :ingredients
 end
