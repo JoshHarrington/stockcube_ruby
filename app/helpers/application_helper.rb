2a5f66991ca8ea119f27601d5fb769715ba11b52
Josh Harrington <josh@joshharrington.co.uk>
2018-03-28 18:17:24 +0100

    add icon support with icomoon and correct asset_path, add colour variables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def icomoon(name)
+		return '<svg class="icomoon-icon icon-'.html_safe + name + '"><use xlink:href="'.html_safe + asset_path('symbol-defs.svg') + '#icon-'.html_safe + name + '"></use></svg>'.html_safe
+	end
 end
