8b799a550d6afff4ce377e0cf9f31e97523a6334
Josh Harrington <josh@joshharrington.co.uk>
2018-04-03 17:53:16 +0100

    delete unused svg files, setup basic fallback to pngs when svgs not supported

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 	def icomoon(name)
-		return '<svg class="icomoon-icon icon-'.html_safe + name + '"><use xlink:href="'.html_safe + asset_path('symbol-defs.svg') + '#icon-'.html_safe + name + '"></use></svg>'.html_safe
+		image_png_name = 'PNG/' + name.to_s + '.png'
+		return '<svg class="icomoon-icon icon-'.html_safe + name + '"><use xlink:href="'.html_safe + asset_path('symbol-defs.svg') + '#icon-'.html_safe + name + '"></use></svg>'.html_safe + '<img class="icon-png" src="'.html_safe + asset_path( image_png_name ) +'"></img>'.html_safe
 	end
 end
