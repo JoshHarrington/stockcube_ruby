52b2a84875737b8efb98fbcff44fd79d727bee07
Josh Harrington <josh@joshharrington.co.uk>
2018-05-13 13:29:10 +0100

    add ingredients edit functionality

--- db/seeds/partials/create_users_cupboards_add_stock.rb
+++ db/seeds/partials/create_users_cupboards_add_stock.rb
@@ -5,7 +5,15 @@ admin_user = User.create(name:  "Admin User",
 			admin: true,
 			activated: true,
       activated_at: Time.zone.now)
-      
+
+chantelle_user = User.create(name:  "Chantelle",
+			email: ENV['CHANTELLE_ADMIN_EMAIL'],
+			password:              ENV['CHANTELLE_ADMIN_PASSWORD'],
+			password_confirmation: ENV['CHANTELLE_ADMIN_PASSWORD'],
+			admin: true,
+			activated: true,
+      activated_at: Time.zone.now)
+
 standard_user = User.create(name:  "Standard User",
 			email: ENV['PERSONAL_STANDARD_EMAIL'],
 			password:              ENV['PERSONAL_PASSWORD'],
@@ -26,17 +34,17 @@ ingredient_picks = Ingredient.all.sample(15)
 ingredient_picks.each_with_index do |ingredient, index|
 
   ingredient_obj = Ingredient.where(id: ingredient.id).first
-  
+
   cupboard_pick = admin_user.cupboards.sample
-  
+
   test_use_by_date = Date.today + 2.weeks + index.days
 
-  stock = Stock.create(    
+  stock = Stock.create(
     amount: 0.1,
     use_by_date: test_use_by_date,
     unit_number: ingredient_obj.unit.unit_number,
     cupboard_id: cupboard_pick.id,
     ingredient_id: ingredient_obj.id
   )
-  
+
 end
