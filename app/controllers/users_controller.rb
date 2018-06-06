2a39168037edc6e8794d66b53b45366cd790788d
Josh Harrington <josh@joshharrington.co.uk>
2018-06-06 23:18:33 +0100

    update user routes to stop everyone getting to the index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :index]
 
   def index
     # @users = User.paginate(page: params[:page])
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
