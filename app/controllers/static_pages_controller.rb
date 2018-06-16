5374ac784bd80466b2f25a0065e6c33a88a64f36
Josh Harrington <josh@joshharrington.co.uk>
2018-06-16 18:30:34 +0100

    fixes to nav on mobile and add banner for demo site

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
 	def home
     if domain_check('demo') == true
+      if logged_in? && current_user && current_user.demo
+        log_out
+      end
       demo_user = User.where(demo: true).first
       if demo_user
         log_in demo_user
