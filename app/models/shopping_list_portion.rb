46b6c6ba483cc738f932de10fa67ee62f38f0a7a
Josh Harrington <josh@joshharrington.co.uk>
2018-07-03 17:13:23 +0100

    add quantity to shopping list portion, set portion amount as float for calcuations

--- app/models/shopping_list_portion.rb
+++ app/models/shopping_list_portion.rb
@@ -2,4 +2,8 @@ class ShoppingListPortion < ApplicationRecord
 	belongs_to :ingredient
 	belongs_to :shopping_list
 	has_one :unit, through: :ingredient
+
+	def quantity
+    Quantity.new(amount, unit.name)
+  end
 end
