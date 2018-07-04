c9841a09ca840445a7e9e455fd62882e7c42322a
Josh Harrington <josh@joshharrington.co.uk>
2018-07-04 08:32:36 +0100

    add attr accessor to shopping list portion model

--- app/models/shopping_list_portion.rb
+++ app/models/shopping_list_portion.rb
@@ -3,6 +3,8 @@ class ShoppingListPortion < ApplicationRecord
 	belongs_to :shopping_list
 	has_one :unit, through: :ingredient
 
+	attr_accessor :ingredient_id, :shopping_list_id, :unit_number, :recipe_number, :portion_amount, :stock_amount, :in_cupboard, :percent_in_cupboard, :checked, :enough_in_cupboard, :plenty_in_cupboard
+
 	def quantity
     Quantity.new(amount, unit.name)
   end
