6d2d96fdc8e0cba08fd7cecc044c73f0bec24ddf
Josh Harrington <josh@joshharrington.co.uk>
2018-08-07 20:58:04 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include ApplicationHelper
-#     if domain_check('demo') == true
-#       if logged_in? && current_user && current_user.demo
-#         log_out
-#       end
-#       if User.where(demo: true).length > 0
-#         demo_user = User.where(demo: true).first
-#         log_in demo_user
-#       end
-#     end
+    if domain_check('demo') == true
+      if logged_in? && current_user && current_user.demo
+        log_out
+      end
+      if User.where(demo: true).length > 0
+        demo_user = User.where(demo: true).first
+        log_in demo_user
+      end
+    end
 end
