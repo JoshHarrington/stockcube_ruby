458bb38ee7320a79cbb5320d5d17248990e78a6a
Josh Harrington <josh@joshharrington.co.uk>
2018-08-07 19:44:50 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include ApplicationHelper
 	
-    if domain_check('demo') == true
-      if logged_in? && current_user && current_user.demo
-        log_out
-      end
-      demo_user = User.where(demo: true).first
-      if demo_user
-        log_in demo_user
-      end
-    end
+#     if domain_check('demo') == true
+#       if logged_in? && current_user && current_user.demo
+#         log_out
+#       end
+#       demo_user = User.where(demo: true).first
+#       if demo_user
+#         log_in demo_user
+#       end
+#     end
 end
