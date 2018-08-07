6ac34f2ac99f982a2a72a4fec55a9bac148e657c
Josh Harrington <josh@joshharrington.co.uk>
2018-08-07 20:57:50 +0100

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
 	def home
-    if domain_check('demo') == true
-      if logged_in? && current_user && current_user.demo
-        log_out
-      end
-      demo_user = User.where(demo: true).first
-      if demo_user
-        log_in demo_user
-      end
-    end
+#     if domain_check('demo') == true
+#       if logged_in? && current_user && current_user.demo
+#         log_out
+#       end
+#       demo_user = User.where(demo: true).first
+#       if demo_user
+#         log_in demo_user
+#       end
+#     end
 	end
 	def about
 	end
