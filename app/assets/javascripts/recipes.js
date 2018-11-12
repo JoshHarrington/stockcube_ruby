c024cd9a3ca5e0bc9c525bbee667e5d5a6c88747
Josh Harrington <josh@joshharrington.co.uk>
2018-11-12 08:36:35 +0000

    update recipe editing to hide publish settings if not available

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -4,16 +4,20 @@ $(document).on("turbolinks:load", function() {
 		confirmDeleteIngredient();
 		togglePublicRowFade();
 		hashFocus();
+		makePublishableOnChange();
 	}
 });
 
 var togglePublicRowFade = function() {
+	var publicStatusRow = $('#recipe_public_status_row');
+	var publicStatusBtns = $('#recipe_public_status_row input');
 	$('#recipe_live_status_row input[type="radio"]').click(function(e){
 		if (e.target == $('#live_radio_1')[0]) {
-			$('#recipe_public_status_row').removeClass('faded_out');
+			publicStatusRow.removeClass('faded_out');
+			publicStatusBtns.prop('disabled', false);
 		} else {
-			$('#recipe_public_status_row').addClass('faded_out');
-
+			publicStatusRow.addClass('faded_out');
+			publicStatusBtns.prop('disabled', true);
 		}
 	})
 }
@@ -57,6 +61,21 @@ var hashFocus = function() {
 		targetElString = hash + ' input, ' + hash + ' textarea';
 		document.querySelectorAll(targetElString)[0].focus();
 	}
+}
+
+var makePublishableOnChange = function() {
+	var publishOptions = $('#publish_options');
+	var publishBtns = $('#publish_options input');
+	$('#recipe_description, #recipe_title_container input, #recipe_cook_time, #recipe_ingredients_list .ingredient_row').change(function(){
+		if(!($('#recipe_description').val() == '' || $('#recipe_title_container input').val() == '' || $('#recipe_cook_time').val() == '' || $('#recipe_ingredients_list .ingredient_row:not(".delete_state")').length == 0)) {
+			console.log('Y: recipe can be saved');
+			publishOptions.removeClass('faded_out');
+			publishBtns.prop('disabled', false);
+		} else {
+			publishOptions.addClass('faded_out')
+			publishBtns.prop('disabled', true);
+		}
+	})
 }
 
 
