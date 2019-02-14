cb2004402d56698961a6d04c504b5cc5fe93e33e
Josh Harrington <josh@joshharrington.co.uk>
2019-02-14 22:13:51 +0000

    update homepage v2, add extra sections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-	def icomoon(name)
+	def icomoon(name, viewBox='')
 		image_png_name = 'PNG/' + name.to_s + '.png'
-		return '<svg class="icomoon-icon icon-'.html_safe + name + '"><use xlink:href="'.html_safe + asset_path('symbol-defs.svg') + '#icon-'.html_safe + name + '"></use></svg>'.html_safe + '<img class="icon-png" src="'.html_safe + asset_path( image_png_name ) +'"></img>'.html_safe
+		return '<svg class="icomoon-icon icon-'.html_safe + name + '"'.html_safe + (!viewBox.empty? ? ' viewBox="'.html_safe + viewBox.to_s.html_safe + '"'.html_safe : '' ) + '><use xlink:href="'.html_safe + asset_path('symbol-defs.svg') + '#icon-'.html_safe + name + '"></use></svg>'.html_safe + '<img class="icon-png" src="'.html_safe + asset_path( image_png_name ) +'"></img>'.html_safe
 	end
-	def large_svg(name, viewbox='')
-		return '<svg class="large-svg '.html_safe + name + '"'.html_safe + (!viewbox.empty? ? ' viewBox="'.html_safe + viewbox.to_s.html_safe + '"'.html_safe : '' )  + '><use xlink:href="'.html_safe + asset_path( name + '.svg') + '#'.html_safe + name + '"></use></svg>'.html_safe
+	def large_svg(name, viewBox='')
+		return '<svg class="large-svg '.html_safe + name + '"'.html_safe + (!viewBox.empty? ? ' viewBox="'.html_safe + viewBox.to_s.html_safe + '"'.html_safe : '' )  + '><use xlink:href="'.html_safe + asset_path( name + '.svg') + '#'.html_safe + name + '"></use></svg>'.html_safe
 	end
 	def random_name
 		names = ["🤖 Ursula K. Le Guin",
