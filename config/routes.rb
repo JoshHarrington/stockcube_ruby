00beaee6a972081154c3721748d80fb4488b7958
Josh Harrington <josh@joshharrington.co.uk>
2019-02-18 18:11:22 +0000

    quick fix for redirect to cupboards after cupboard edit fails

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Rails.application.routes.draw do
   get '/cupboards/accept_cupboard_invite' => 'cupboards#accept_cupboard_invite'
   post '/cupboards/accept_cupboard_invite' => 'cupboards#accept_cupboard_invite', as: :accept_cupboard_invite
   get '/cupboards/sharing_request' => 'cupboards#sharing_request', as: :cupboard_sharing_request
-  get '/cupboards/:id' => 'cupboards#show', as: :cupboard
+  get '/cupboards/:id', to: redirect('/cupboards')
   get '/cupboards/:id/edit' => 'cupboards#edit', as: :edit_cupboard
   patch '/cupboards/:id' => 'cupboards#update'
   post '/cupboards' => 'cupboards#create'
