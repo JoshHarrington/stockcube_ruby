791f8a7a0251c6347a5beaf7dce1b8965a193929
Josh Harrington <josh@joshharrington.co.uk>
2019-02-25 17:50:35 +0000

    fixes for alert js

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -12,51 +12,26 @@ $(document).ready(function() {
 
 
 		var standardAlerts = document.querySelectorAll('.alert:not(.alert-sticky)');
-		var maxIntervalsToRun = standardAlerts.length - 1
+		var maxIntervalsToRun = standardAlerts.length
 
-		var i = 0;
-		console.log(i, standardAlerts[i]);
+		var counter = 0;
 		var interval = setInterval(function(){
-			hideStandardAlerts(standardAlerts, i)
-			// need to update the counter inside the timeout else it gets the wrong value
-			i++;
-			if(i === maxIntervalsToRun) {
+			hideAlert(standardAlerts[counter]);
+			counter++;
+			if(counter === maxIntervalsToRun) {
 				clearInterval(interval);
 			}
 		}, 4000);
 
-		function hideStandardAlerts(standardAlerts, i){
-			console.log(i, standardAlerts[i]);
-			hideAlert(standardAlerts[i]);
-		}
-
 		var noUpdateBtns = document.querySelectorAll('.dismiss_no_update');
 		for (var i = 0; i < noUpdateBtns.length; i++) {
 			noUpdateBtns[i].addEventListener('click', function(e) {
-				console.log(e.target)
 				hideAlert(e.target)
 				if (e.target.classList.contains('recipe_no_update_cookie_set')){
 					noRecipeUpdate();
 				}
 			});
 		}
-
-
-		// setTimeout(function() {
-		// 	clearNotice();
-		// }, 6000);
-		// if (!document.cookie.split(';').filter(function(item) {
-		// 	return item.trim().indexOf('recipeUpdate=nope') == 0
-		// }).length) {
-		// 	document.querySelector('.alert-sticky').style.display = 'block'
-		// }
-		// var noUpdateBtns = document.querySelectorAll('.dismiss_no_update');
-		// for (var i = 0; i < noUpdateBtns.length; i++) {
-		// 	noUpdateBtns[i].addEventListener('click', function(e) {
-		// 		clearNotice('all', e);
-		// 		noRecipeUpdate();
-		// 	});
-		// }
 	}
 });
 
