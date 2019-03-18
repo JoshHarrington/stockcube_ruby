0dc2a72e43ef03d0ba93060871c299985b9301a2
Josh Harrington <josh@joshharrington.co.uk>
2019-03-18 22:53:03 +0000

    switch portions over to standalone units

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,11 +1,6 @@
 class Unit < ApplicationRecord
   has_many :ingredients
-  has_many :portions, through: :ingredients
+  has_one :portion
   has_one :stock
   has_one :user_fav_stock
-
-  accepts_nested_attributes_for :portions,
-  :reject_if => :all_blank,
-  :allow_destroy => true
-  accepts_nested_attributes_for :ingredients
 end
