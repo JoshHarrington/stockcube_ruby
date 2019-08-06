4b0da44719c6926018fd895ee825ae77c9c2fd9e
Josh Harrington <josh@joshharrington.co.uk>
2019-08-06 19:31:05 +0100

    setup basic dashboard page

--- app/webpacker/functions/dashboard.js
+++ app/webpacker/functions/dashboard.js
@@ -1,9 +1,10 @@
 import { ready } from './utils'
 import Sortable from 'sortablejs'
+import {tns} from 'tiny-slider/src/tiny-slider'
 
 const dashboardFn = () => {
 	const recipeList = document.querySelector('[data-recipe-list]')
-	const planner = document.querySelectorAll('[data-planner] .list_block')
+	const plannerBlocks = document.querySelectorAll('[data-planner] .tiny-drop')
 	new Sortable.create(recipeList, {
 		group: {
 			name: 'recipe-sharing',
@@ -12,7 +13,7 @@ const dashboardFn = () => {
 		},
 		sort: false
 	})
-	planner.forEach(function(dayBlock, i){
+	plannerBlocks.forEach(function(dayBlock){
 		new Sortable.create(dayBlock, {
 			group: {
 				name: 'recipe-sharing',
@@ -20,8 +21,19 @@ const dashboardFn = () => {
 				put: true
 			}
 		})
-
+	})
+  const slider = tns({
+    container: '[data-planner]',
+    items: 4,
+		slideBy: 'page',
+		mouseDrag: true,
+		loop: false,
+		gutter: 10,
+		edgePadding: 40,
+		arrowKeys: true,
+		swipeAngle: false,
 	});
+
 }
 
 ready(dashboardFn)
