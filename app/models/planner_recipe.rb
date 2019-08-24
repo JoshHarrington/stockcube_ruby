d7beb296fca1d30f8b226c119b6558af2266dbd1
Josh Harrington <josh@joshharrington.co.uk>
2019-08-24 11:53:04 +0100

    Set stock to get a nil planner_recipe_id on destroy

--- app/models/planner_recipe.rb
+++ app/models/planner_recipe.rb
@@ -2,4 +2,5 @@ class PlannerRecipe < ApplicationRecord
 	belongs_to :recipe
 	belongs_to :user
 	has_many :planner_shopping_list_portions, dependent: :delete_all
+	has_many :stocks, dependent: :nullify
 end
