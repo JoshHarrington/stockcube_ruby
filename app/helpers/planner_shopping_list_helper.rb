b37d99fc07b3b3cb5f56ecfe45ab45f20e6b188f
Josh Harrington <josh@joshharrington.co.uk>
2019-09-03 23:45:07 +0100

    Use ingredient health check to only compare healthy ings

--- app/helpers/planner_shopping_list_helper.rb
+++ app/helpers/planner_shopping_list_helper.rb
@@ -11,6 +11,8 @@ module PlannerShoppingListHelper
 		stock = cupboards.map{|c| c.stocks.select{|s| s.use_by_date > Date.current - 4.days && s.ingredient.name.downcase != 'water' && s.planner_recipe_id == nil }}.flatten.compact
 		ingredients_from_stock = stock.map(&:ingredient_id)
 
+		uncommon_ingredients = ingredients_from_recipe_portions - ingredients_from_stock
+
 		if ingredients_from_recipe_portions && ingredients_from_stock.length > 0
 			common_ingredients = ingredients_from_recipe_portions & ingredients_from_stock
 			common_ingredients.each do |ing|
@@ -21,50 +23,53 @@ module PlannerShoppingListHelper
 				ingredient_plus_diff_amount = ingredient_plus_difference(stock_from_ing, portion_from_ing)
 				default_unit_id_output = default_unit_id(portion_from_ing)
 
-				if ingredient_plus_diff_amount <= 0
-					stock_from_ing.update_attributes(
-						planner_recipe_id: planner_recipe.id
-					)
-					## original stock updated
-					if ingredient_plus_diff_amount < 0
-						##  if amount < 0 new stock is needed so shopping list portion should be created at the same time
-						PlannerShoppingListPortion.create(
-							user_id: current_user.id,
-							planner_recipe_id: planner_recipe.id,
-							ingredient_id: ing,
-							unit_id: default_unit_id_output,
-							amount: -(ingredient_plus_diff_amount),
-							planner_shopping_list_id: planner_shopping_list.id
+				if ingredient_plus_diff_amount
+					if ingredient_plus_diff_amount <= 0
+						stock_from_ing.update_attributes(
+							planner_recipe_id: planner_recipe.id
 						)
+						## original stock updated
+						if ingredient_plus_diff_amount < 0
+							## when amount < 0 new stock is needed so shopping list portion should be created at the same time
+							PlannerShoppingListPortion.create(
+								user_id: current_user.id,
+								planner_recipe_id: planner_recipe.id,
+								ingredient_id: ing,
+								unit_id: default_unit_id_output,
+								amount: -(ingredient_plus_diff_amount),
+								planner_shopping_list_id: planner_shopping_list.id
+							)
+						end
+					elsif ingredient_plus_diff_amount > 0
+						recipe_stock = Stock.create(
+							ingredient_id: ing,
+							amount: ingredient_plus_converter(portion_from_ing),
+							planner_recipe_id: planner_recipe.id,
+							unit_id: default_unit_id_output,
+							use_by_date: stock_from_ing.use_by_date,
+							cupboard_id: stock_from_ing.cupboard_id,
+							hidden: false,
+							always_available: false
+						)
+						current_user.stocks << recipe_stock
+						ingredient_plus_hash = {
+							unit_metric_ratio: stock_from_ing.unit.metric_ratio,
+							unit_type: stock_from_ing.unit.unit_type,
+							amount: ingredient_plus_diff_amount
+						}
+
+						stock_from_ing.update_attributes(
+							unit_id: default_unit_id_output,
+							amount: ingredient_plus_diff_amount
+						)
+
 					end
-				elsif ingredient_plus_diff_amount > 0
-					recipe_stock = Stock.create(
-						ingredient_id: ing,
-						amount: ingredient_plus_converter(portion_from_ing),
-						planner_recipe_id: planner_recipe.id,
-						unit_id: default_unit_id_output,
-						use_by_date: stock_from_ing.use_by_date,
-						cupboard_id: stock_from_ing.cupboard_id,
-						hidden: false,
-						always_available: false
-					)
-					current_user.stocks << recipe_stock
-					ingredient_plus_hash = {
-						unit_metric_ratio: stock_from_ing.unit.metric_ratio,
-						unit_type: stock_from_ing.unit.unit_type,
-						amount: ingredient_plus_diff_amount
-					}
-
-					stock_from_ing.update_attributes(
-						unit_id: default_unit_id_output,
-						amount: ingredient_plus_diff_amount
-					)
-
+				else
+					uncommon_ingredients.push(ing)
 				end
 			end
 		end
 
-		uncommon_ingredients = ingredients_from_recipe_portions - ingredients_from_stock
 		uncommon_ingredients.each do |uc_ing|
 			portion_from_uc_ing = recipe_portions.select{|p| p.ingredient_id == uc_ing}.first
 			PlannerShoppingListPortion.create(
