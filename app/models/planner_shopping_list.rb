cca10b1f80f189e33a4524293ea4e3ad7d220c49
Josh Harrington <josh@joshharrington.co.uk>
2019-09-07 17:28:43 +0100

    Add reference to combi portions

--- app/models/planner_shopping_list.rb
+++ app/models/planner_shopping_list.rb
@@ -1,4 +1,5 @@
 class PlannerShoppingList < ApplicationRecord
 	belongs_to :user
 	has_many :planner_shopping_list_portions, dependent: :delete_all
+	has_many :combi_planner_shopping_list_portions, dependent: :delete_all
 end
