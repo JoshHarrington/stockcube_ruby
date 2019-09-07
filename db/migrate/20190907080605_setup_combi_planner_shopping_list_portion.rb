aa14c79bd7caa3e2c5d74a8ed937b4fa5b74a633
Josh Harrington <josh@joshharrington.co.uk>
2019-09-07 17:30:01 +0100

    Add combi portion migrations

--- db/migrate/20190907080605_setup_combi_planner_shopping_list_portion.rb
+++ db/migrate/20190907080605_setup_combi_planner_shopping_list_portion.rb
@@ -1,6 +1,6 @@
 class SetupCombiPlannerShoppingListPortion < ActiveRecord::Migration[5.1]
   def change
-    create_table :combi_planner_sl_portions do |t|
+    create_table :combi_planner_shopping_list_portions do |t|
       t.belongs_to :user, foreign_key: true
       t.belongs_to :planner_shopping_list, foreign_key: true, index: {:name => "index_combi_planner_shopping_list_portions"}
       t.belongs_to :ingredient, foreign_key: true
