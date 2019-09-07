66fef760a451c90f21d521176a205b5a7276a878
Josh Harrington <josh@joshharrington.co.uk>
2019-09-07 09:04:44 +0100

    Setup ingredient matching functions

--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,18 +1,29 @@
 module IngredientsHelper
-	def ingredient_health_check(ingredient_plus)
-		if (ingredient_plus.class == Hash && ingredient_plus.has_key?(:unit_metric_ratio)) ||
-			(ingredient_plus.class != Hash && ingredient_plus.unit && ingredient_plus.unit.metric_ratio)
-			return ingredient_plus
+	def get_ingredient_plus_unit_type(ingredient_plus)
+		if ingredient_plus.class == Hash && ingredient_plus.has_key?(:unit_type)
+			return ingredient_plus[:unit_type]
+		elsif ingredient_plus.class != Hash && ingredient_plus.unit && ingredient_plus.unit.unit_type
+			return ingredient_plus.unit.unit_type
 		else
 			return false
 		end
+	end
+
+	def ingredient_health_check(ingredient_plus)
+		return false unless (ingredient_plus.class == Hash && ingredient_plus.has_key?(:unit_metric_ratio)) ||
+			(ingredient_plus.class != Hash && ingredient_plus.unit && ingredient_plus.unit.metric_ratio)
+		return ingredient_plus
+	end
+
+	def ingredient_metric_match(ingredient_plus_1, ingredient_plus_2)
+		return false unless get_ingredient_plus_unit_type(ingredient_plus_1) == get_ingredient_plus_unit_type(ingredient_plus_2)
 	end
 
 	def ingredient_plus_converter(ingredient_plus = nil)
 		## ingredient_plus means either stock or portion
 		## or anything which has an ingredient association and an amount
 
-		return unless ingredient_plus != nil &&
+		return false unless ingredient_plus != nil &&
 			(ingredient_plus.class == Hash && ingredient_plus.has_key?(:unit_metric_ratio) &&
 			ingredient_plus.has_key?(:unit_type) && ingredient_plus.has_key?(:amount)) ||
 			(ingredient_plus.class != Hash && ingredient_plus.unit && ingredient_plus.unit.metric_ratio &&
@@ -30,30 +41,22 @@ module IngredientsHelper
 	end
 
 	def ingredient_plus_difference(ingredient_plus_1 = nil, ingredient_plus_2 = nil)
-		if ingredient_health_check(ingredient_plus_1) && ingredient_health_check(ingredient_plus_2)
-			return ingredient_plus_converter(ingredient_plus_1) - ingredient_plus_converter(ingredient_plus_2)
-		else
-			return false
-		end
-
+		return false unless ingredient_metric_match(ingredient_plus_1, ingredient_plus_2)
+		return false unless ingredient_health_check(ingredient_plus_1) && ingredient_health_check(ingredient_plus_2)
+		return ingredient_plus_converter(ingredient_plus_1) - ingredient_plus_converter(ingredient_plus_2)
 	end
 
 	def ingredient_plus_addition(ingredient_plus_1 = nil, ingredient_plus_2 = nil)
+		return false unless ingredient_metric_match(ingredient_plus_1, ingredient_plus_2)
+		return false unless ingredient_health_check(ingredient_plus_1) && ingredient_health_check(ingredient_plus_2)
 		return ingredient_plus_converter(ingredient_plus_1) + ingredient_plus_converter(ingredient_plus_2)
 	end
 
 	def default_unit_id(ingredient_plus = nil)
-		return unless ingredient_plus != nil &&
-		(ingredient_plus.class == Hash && ingredient_plus.has_key?(:unit_metric_ratio) &&
-		ingredient_plus.has_key?(:unit_type) && ingredient_plus.has_key?(:amount)) ||
-		(ingredient_plus.class != Hash && ingredient_plus.unit && ingredient_plus.unit.metric_ratio &&
-		ingredient_plus.unit.unit_type)
+		return unless ingredient_plus != nil
+		return unless ingredient_health_check(ingredient_plus) && get_ingredient_plus_unit_type(ingredient_plus)
 
-		if (ingredient_plus.class != Hash && ingredient_plus.unit.unit_type == 'mass') || (ingredient_plus.class == Hash && ingredient_plus[:unit_type] == 'mass')
-			return Unit.find_by(unit_type: 'mass', metric_ratio: 1).id
-		elsif (ingredient_plus.class != Hash && ingredient_plus.unit.unit_type == 'volume') || (ingredient_plus.class == Hash && ingredient_plus[:unit_type] == 'volume')
-			return Unit.find_by(unit_type: 'volume', metric_ratio: 1).id
-		end
+		return Unit.find_by(unit_type: get_ingredient_plus_unit_type(ingredient_plus), metric_ratio: 1).id
 
 	end
 
