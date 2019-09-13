09c3a004e33ab60d0dfcb5117145abac7e0359fb
Josh Harrington <josh@joshharrington.co.uk>
2019-09-13 18:11:52 +0100

    Update stock helper to try to run recipe match more efficiently

--- app/helpers/stock_helper.rb
+++ app/helpers/stock_helper.rb
@@ -3,46 +3,42 @@ module StockHelper
 	# if no ingredient_id defined then use all of current users stock to search for matching recipes
 
 
-	def update_recipe_stock_matches_core(ingredient_id = nil, user_id = nil, recipe_ids = nil)
-		two_days_ago = Date.current - 2.days
+	def update_recipe_stock_matches_core(ingredient_ids = nil, user_id = nil, recipe_ids = nil)
+
 		if user_id != nil
 			user_id = user_id
 		elsif current_user
 			user_id = current_user[:id]
+		else
+			return
 		end
 
 		active_cupboard_ids = CupboardUser.where(user_id: user_id, accepted: true).map{|cu| cu.cupboard.id unless cu.cupboard == nil && (cu.cupboard.setup == true || cu.cupboard.hidden == true) }.compact
-		cupboard_stock_in_date_ingredient_ids = Stock.where(cupboard_id: active_cupboard_ids, hidden: false, planner_recipe_id: nil).where("use_by_date >= :date", date: two_days_ago).uniq { |s| s.ingredient_id }.map{ |s| s.ingredient.id }.compact
+		cupboard_stock_in_date_ingredient_ids = Stock.where(cupboard_id: active_cupboard_ids, hidden: false, planner_recipe_id: nil).where("use_by_date >= :date", date: Date.current - 2.days).map{ |s| s.ingredient_id }.uniq.compact
 
-		if recipe_ids == nil
-			if ingredient_id.class == Integer
-				ingredient_name = Ingredient.find(ingredient_id)[:name]
-				recipes = Recipe.search(ingredient_name, fields: ["ingredient_names^100"]).results.uniq
-			elsif ingredient_id.class == Array
-				ingredient_name = Ingredient.find(ingredient_id.uniq).map(&:name)
-				recipes = Recipe.search(ingredient_name, operator: "or", fields: ["ingredient_names^100"]).results.uniq
-			else
-				recipes = Recipe.all.uniq
-			end
-			recipes.each do |recipe|
-				recipe_stock_update(recipe, cupboard_stock_in_date_ingredient_ids, user_id)
-			end
-		elsif recipe_ids != nil
-			if recipe_ids.class == Integer
-				recipe = Recipe.find(recipe_ids)
-				recipe_stock_update(recipe, cupboard_stock_in_date_ingredient_ids, user_id)
-			elsif recipe_ids.class == Array
-				recipes = Recipe.find(recipe_ids)
-				recipes.each do |recipe|
-					recipe_stock_update(recipe, cupboard_stock_in_date_ingredient_ids, user_id)
-				end
-			end
+		filtered_ingredient_ids = [].push(ingredient_ids).uniq.flatten.compact
+		filtered_recipe_ids = [].push(recipe_ids).uniq.flatten.compact
+
+		search_recipes = []
+		if filtered_recipe_ids.length > 0
+			collected_recipes = Recipe.find(filtered_recipe_ids)
+		elsif filtered_ingredient_ids.length > 0
+			ingredient_names = Ingredient.find(filtered_ingredient_ids)[:name]
+			search_recipes = Recipe.search(ingredient_name, operator: "or", fields: ["ingredient_names^100"]).results.uniq
+		end
+		if filtered_recipe_ids.length == 0 && filtered_ingredient_ids.length == 0
+			collected_recipes = Recipe.all.uniq.reject{|r| !r.portions || r.portions.length == 0}
+		end
+
+		all_recipes = collected_recipes + search_recipes
+		all_recipes.each do |recipe|
+			recipe_stock_update(recipe, cupboard_stock_in_date_ingredient_ids, user_id)
 		end
 	end
 
-	def	update_recipe_stock_matches(ingredient_id = nil, user_id = nil, recipe_ids = nil)
+	def	update_recipe_stock_matches(ingredient_ids = nil, user_id = nil, recipe_ids = nil)
 		return unless current_user.user_recipe_stock_matches.order("updated_at desc").first.updated_at < 20.minutes.ago
-		update_recipe_stock_matches_core(ingredient_id, user_id, recipe_ids)
+		update_recipe_stock_matches_core(ingredient_ids, user_id, recipe_ids)
 
 		flash[:info] = %Q[We've updated your <a href="/recipes">recipe list</a> based on your stock so you can see the quickest recipes to make]
 
@@ -67,21 +63,25 @@ module StockHelper
 		)
 	end
 
-	def add_individual_portion_as_stock(portion)
+	def add_individual_portion_as_stock(portions)
+
+		portions_array = [].push(portions).flatten
 
 		cupboard_id = current_user.cupboard_users.where(accepted: true).select{|cu| cu.cupboard.setup == false && cu.cupboard.hidden == false }.map{|cu| cu.cupboard }.sort_by{|c| c.updated_at}.first.id
-
-		recipe_stock = Stock.create(
-			ingredient_id: portion.ingredient_id,
-			amount: portion.amount,
-			planner_recipe_id: portion.planner_recipe_id,
-			unit_id: portion.unit_id,
-			use_by_date: Date.current + 2.weeks,
-			cupboard_id: cupboard_id,
-			hidden: false,
-			always_available: false
-		)
-		current_user.stocks << recipe_stock
+		portions_array.each do |portion|
+			recipe_stock = Stock.create(
+				ingredient_id: portion.ingredient_id,
+				amount: portion.amount,
+				planner_recipe_id: portion.planner_recipe_id,
+				unit_id: portion.unit_id,
+				use_by_date: Date.current + 2.weeks,
+				cupboard_id: cupboard_id,
+				hidden: false,
+				always_available: false
+			)
+			current_user.stocks << recipe_stock
+			portion.destroy
+		end
 
 	end
 
