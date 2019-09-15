5240cc3844b0a0618563d277bf447345489ad3cd
Josh Harrington <josh@joshharrington.co.uk>
2019-09-15 20:08:50 +0100

    Only setup event listener if delete btn on page

--- app/webpacker/functions/stocks.js
+++ app/webpacker/functions/stocks.js
@@ -2,12 +2,14 @@ import { ready } from "./utils";
 
 const deleteStockBtnFunction = () => {
   const stockDeleteBtn = document.querySelector('#stock_delete_btn')
-  stockDeleteBtn.addEventListener('click', (e) => {
-    const confirmed = confirm("Are you sure you want to delete this ingredient from your cupboards?")
-    if (!confirmed) {
-      e.preventDefault()
-    }
-  })
+  if (stockDeleteBtn) {
+    stockDeleteBtn.addEventListener('click', (e) => {
+      const confirmed = confirm("Are you sure you want to delete this ingredient from your cupboards?")
+      if (!confirmed) {
+        e.preventDefault()
+      }
+    })
+  }
 }
 
 const stockPageFunctions = () => {
