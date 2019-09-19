ea29f0742797e592d34e0e7d1f9a5cf641025dda
Josh Harrington <josh@joshharrington.co.uk>
2019-09-19 23:30:17 +0100

    Set gen_id column to be string not bigint

--- db/migrate/20190918213656_add_gen_id_to_shopping_list.rb
+++ db/migrate/20190918213656_add_gen_id_to_shopping_list.rb
@@ -1,6 +1,6 @@
 class AddGenIdToShoppingList < ActiveRecord::Migration[5.1]
   def change
-    add_column :planner_shopping_lists, :gen_id, :bigint
+    add_column :planner_shopping_lists, :gen_id, :string
     add_index :planner_shopping_lists, :gen_id, unique: true
   end
 end
