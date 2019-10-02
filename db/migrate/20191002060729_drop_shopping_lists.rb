f0eb193a3efef7af2c43d099c25dc5850d2076ba
Josh Harrington <josh@joshharrington.co.uk>
2019-10-02 09:58:47 +0100

    Fix typo in shopping list name when dropping table

--- db/migrate/20191002060729_drop_shopping_lists.rb
+++ db/migrate/20191002060729_drop_shopping_lists.rb
@@ -2,6 +2,6 @@ class DropShoppingLists < ActiveRecord::Migration[5.1]
   def change
     drop_table :shopping_list_portions
     drop_table :shopping_list_recipes
-    drop_table :shopping_list
+    drop_table :shopping_lists
   end
 end
