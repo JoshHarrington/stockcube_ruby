d76bd84dbd977fefd04e9f8697b69d51088a84e6
Josh Harrington <josh@joshharrington.co.uk>
2019-10-17 02:27:09 +0100

    Remove jquery from feedback scroll to function

--- app/webpacker/functions/feedback.js
+++ app/webpacker/functions/feedback.js
@@ -1,15 +1,17 @@
-var scroll_to_form = function() {
-	$('.feedback_form_label').click(function(){
-		if ($('.feedback_form_button.hidden_checkbox:checked').length == 0) {
-			document.querySelector('.footer_feedback_form--wrapper').scrollIntoView({
-				behavior: 'smooth'
-			});
-		}
-	});
+import { ready } from "./utils";
+
+const scrollToForm = () => {
+	document.querySelector('.feedback_form_label').addEventListener('click', () => {
+		document.querySelector('.footer_feedback_form--wrapper').scrollIntoView({
+			behavior: 'smooth'
+		});
+	})
 }
 
-$(document).ready(function() {
-	if ($('#footer-feedback-form').length > 0) {
-		scroll_to_form();
+const feedback = () => {
+	if (document.querySelector('.footer_feedback_form--wrapper')) {
+		scrollToForm()
 	}
-});
+}
+
+ready(feedback)
