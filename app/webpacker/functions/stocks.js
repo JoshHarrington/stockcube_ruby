ec32757829fb99836f35ed5daf036035a362b679
Josh Harrington <josh@joshharrington.co.uk>
2019-12-31 17:06:49 +0000

    Setup typical stock adding use by date

--- app/webpacker/functions/stocks.js
+++ app/webpacker/functions/stocks.js
@@ -12,8 +12,42 @@ const deleteStockBtnFunction = () => {
   }
 }
 
+const updateTypicalIngredientUseByDate = (target) => {
+  const IngredientBlock = target.closest('.list_block.list_block__column')
+  const value = target.value
+  const useByDateObj = new Date((new Date()).getTime() + (Number(value) * (60*60*24*1000)))
+  let useByDateMonthString = String(Number(useByDateObj.getUTCMonth()) + 1)
+  if (useByDateMonthString.length < 2){
+    useByDateMonthString = "0" + useByDateMonthString
+  }
+  let useByDateDayString = String(useByDateObj.getUTCDate())
+  if (useByDateDayString.length < 2){
+    useByDateDayString = "0" + useByDateDayString
+  }
+
+  const useByDateString = useByDateObj.getUTCFullYear() + '-' + useByDateMonthString + '-' + useByDateDayString
+
+  const useByDateFields = IngredientBlock.querySelectorAll('input[name="stock[use_by_date]"]')
+
+  useByDateFields.forEach((field) => {
+    field.value = useByDateString
+  })
+}
+
+const watchForUseByDateChange = () => {
+  if(document.querySelector('.stocks_controller.new_page')){
+    const UpdateUseByDateInputs = document.querySelectorAll('.set_use_by_date_diff')
+    UpdateUseByDateInputs.forEach((Input) => {
+      Input.addEventListener('change', (e) => {
+        updateTypicalIngredientUseByDate(e.target)
+      })
+    })
+  }
+}
+
 const stockPageFunctions = () => {
   deleteStockBtnFunction()
+  watchForUseByDateChange()
 }
 
 ready(stockPageFunctions)
