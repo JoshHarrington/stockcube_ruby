f7c605739577da6e96b7fedc37869405e8d40444
Josh Harrington <josh@joshharrington.co.uk>
2020-01-04 18:33:12 +0000

    Allow edited and adding default ingredient sizes

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -33,11 +33,32 @@ class IngredientsController < ApplicationController
 		@units = unit_list()
 	end
 	def update
-		@ingredient = Ingredient.find(params[:id])
-		@units = unit_list()
+		ingredient = Ingredient.find(params[:id])
 
-		@portions = @ingredient.portions
-		if @ingredient.update(ingredient_params)
+		if params.has_key?(:default_ingredient_sizes)
+			sizes_hash = params[:default_ingredient_sizes].to_unsafe_h
+			sizes_hash.map do |size_id, values|
+				if size_id === "new"
+					values.each do |value|
+						if value["amount"].to_f != 0.0
+							ingredient.default_ingredient_sizes.create(
+								amount: value["amount"],
+								unit_id: value["unit_id"]
+							)
+						end
+					end
+				else
+					if ingredient.default_ingredient_sizes.where(id: size_id).length > 0
+						ingredient.default_ingredient_sizes.where(id: size_id).first.update_attributes(
+							amount: values["amount"],
+							unit_id: values["unit_id"]
+						)
+					end
+				end
+			end
+		end
+
+		if ingredient.update(ingredient_params)
 			redirect_to ingredients_path
 			Ingredient.reindex
 		else
@@ -50,7 +71,7 @@ class IngredientsController < ApplicationController
 
 	protected
 		def ingredient_params
-			params.require(:ingredient).permit(:name, :searchable, :vegan, :vegetarian, :gluten_free, :dairy_free, :kosher, :unit_id)
+			params.require(:ingredient).permit(:name, :searchable, :vegan, :vegetarian, :gluten_free, :dairy_free, :kosher)
 		end
 
 		# Confirms an admin user.
--- app/webpacker/functions/ingredients.js
+++ app/webpacker/functions/ingredients.js
@@ -1,7 +1,80 @@
 import { ready } from './utils'
+import Choices from 'choices.js'
 
-const IngredientsFn = () => {
-	// DateUpdate()
+const WatchForAddNewSizeBtnClick = () => {
+	const NewSizeBtn = document.querySelector('#new_size_button')
+	const ClonedRows = document.querySelectorAll('.cloned_row')
+
+	if (NewSizeBtn) {
+		if (ClonedRows.length > 0) {
+			const lastClonedRow = ClonedRows[ClonedRows.length - 1]
+			lastClonedRowValue = lastClonedRow.querySelector('input[type="number"]').value
+			if (lastClonedRowValue !== "" && lastClonedRowValue !== "0" ) {
+				NewSizeBtn.addEventListener('click', InsertNewSizeRow, {once: true})
+			}
+		} else {
+			NewSizeBtn.addEventListener('click', InsertNewSizeRow, {once: true})
+		}
+	}
+}
+
+const InsertNewSizeRow = (e) => {
+	const NewSizeBtn = e.target
+	DisableNewSizeBtn(NewSizeBtn)
+	const RowForCloning = document.querySelector('.row_for_cloning')
+	const NewSizeButtonRow = document.querySelector('.new_size_button_row')
+	if (RowForCloning && NewSizeButtonRow) {
+		const CloneRow = RowForCloning.cloneNode(true)
+
+		CloneRow.classList.remove('row_for_cloning')
+		CloneRow.classList.add('cloned_row')
+
+		// CloneRow.getAttribute('data-id-start')
+
+		const CloneRowSelect = CloneRow.querySelector('select')
+		const choicesSelect = new Choices(CloneRowSelect, {
+			classNames: {
+				containerOuter: 'choices choices_select'
+			}
+		})
+
+		CloneRow.style.display = 'flex'
+		const SizesUL = RowForCloning.parentNode
+		SizesUL.insertBefore(CloneRow, NewSizeButtonRow)
+		WatchForInputNumberChange(CloneRow, NewSizeBtn)
+	}
+}
+
+const DisableNewSizeBtn = (NewSizeBtn) => {
+	NewSizeBtn.removeEventListener('click', InsertNewSizeRow, false)
+	NewSizeBtn.style.borderColor = "#cccccc"
+	NewSizeBtn.style.color = "#888888"
+	NewSizeBtn.style.cursor = "not-allowed"
+	NewSizeBtn.addEventListener('click', FocusLastClonedInput, false)
+}
+
+const WatchForInputNumberChange = (CloneRow, NewSizeBtn) => {
+	CloneRow.querySelector('input[type="number"]').addEventListener('change', () => EnableNewSizeBtn(NewSizeBtn), {once: true})
+}
+
+const EnableNewSizeBtn = (NewSizeBtn) => {
+	NewSizeBtn.addEventListener('click', InsertNewSizeRow, {once: true})
+	NewSizeBtn.style.borderColor = "#93dbd7"
+	NewSizeBtn.style.color = "black"
+	NewSizeBtn.style.cursor = "pointer"
+	NewSizeBtn.removeEventListener('click', FocusLastClonedInput, false)
+}
+
+const FocusLastClonedInput = () => {
+	const ClonedRows = document.querySelectorAll('.cloned_row')
+	if (ClonedRows.length > 0) {
+		const lastClonedRow = ClonedRows[ClonedRows.length - 1]
+		const lastClonedRowInput = lastClonedRow.querySelector('input[type="number"]')
+		const lastClonedRowValue = lastClonedRowInput.value
+		if (lastClonedRowValue === "" || lastClonedRowValue === "0" ) {
+			lastClonedRowInput.focus()
+		}
+	}
 }
 
 const DateUpdate = () => {
@@ -15,6 +88,11 @@ const DateUpdate = () => {
 			})
 		})
 	}
+}
+
+const IngredientsFn = () => {
+	// DateUpdate()
+	WatchForAddNewSizeBtnClick()
 }
 
 ready(IngredientsFn)
