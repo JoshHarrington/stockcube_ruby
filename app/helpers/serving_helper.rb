0765600f74a3fe0226134fbcc082faf8d8a8ec97
Josh Harrington <josh@joshharrington.co.uk>
2020-04-29 09:35:48 +0100

    Setup combi serving description - combine portions Update short serving size to work for hashes

--- app/helpers/serving_helper.rb
+++ app/helpers/serving_helper.rb
@@ -88,24 +88,76 @@ module ServingHelper
 		if combi_portion.amount != nil && combi_portion.unit_id != nil
 			short_serving_description(combi_portion)
 		else
-			return combi_portion.planner_shopping_list_portions.map{|p|
-				standard_serving_size(p)
-			}.join(" + ").to_s + ' ' + combi_portion.ingredient.name
+
+			combi_serving_portions_formatted = []
+
+			grouped_combi_portions__by_unit_type = combi_portion.planner_shopping_list_portions.select{|p|p.unit.unit_type != nil}.group_by{|p| [p.ingredient_id, p.unit.unit_type]}
+			grouped_combi_portions__by_unit_type.each do |(ingredient_id, unit_type), portions|
+				if portions.length == 1
+					combi_serving_portions_formatted.push({
+						amount: portions.first.amount,
+						unit: portions.first.unit
+					})
+				elsif portions.group_by{|p| p.unit_id}.length == 1
+					combi_serving_portions_formatted.push({
+						amount: portions.map{|p|p.amount}.sum,
+						unit: portions.first.unit
+					})
+				else
+					portions_amount_array = portions.map{ |p|
+						standardise_amount_with_metric_ratio(p.amount, p.unit.metric_ratio)
+					}
+					combi_serving_portions_formatted.push({
+						amount: portions_amount_array.sum / portions.first.unit.metric_ratio,
+						unit: portion.first.unit
+					})
+				end
+			end
+
+			grouped_non_metric_combi_portions__by_unit_id = combi_portion.planner_shopping_list_portions.select{|p|p.unit.unit_type == nil}.group_by{|p| [p.ingredient_id, p.unit_id]}
+
+			grouped_non_metric_combi_portions__by_unit_id.each do |(ingredient_id, unit_id), portions|
+				if portions.length == 1
+					combi_serving_portions_formatted.push({
+						amount: portions.first.amount,
+						unit: portions.first.unit
+					})
+				else
+					combi_serving_portions_formatted.push({
+						amount: portions.map{|p|p.amount}.sum,
+						unit: portions.first.unit
+					})
+				end
+			end
+
+			combi_portions_list = combi_serving_portions_formatted.map{|p| short_serving_size(p) }
+
+
+			return combi_portions_list.join(" + ").to_s + ' ' + combi_portion.ingredient.name
 		end
 	end
 
-	def standard_serving_size(serving)
-		updated_serving = serving_unit_convert(serving.amount, serving.unit)
+	def short_serving_size(serving = nil)
+		return if serving == nil
+		if serving.class == Hash
+			unit = serving[:unit]
+			amount = serving[:amount]
+		else
+			unit = serving.unit
+			amount = serving.unit
+		end
 
-		unit_name = updated_serving[:unit][:name]
-		amount = updated_serving[:amount]
+		rounded_amount_string = round_if_whole(amount).to_s
+
+		unit_name = unit.name
+		unit_short_name = unit.short_name
 
 		if unit_name.downcase == 'each'
-			return amount.to_s
+			return rounded_amount_string
 		elsif unit_name.downcase == 'small' || unit_name.downcase == 'medium' || unit_name.downcase == 'large'
-			return amount.to_s + ' ' + unit_name
+			return rounded_amount_string + ' ' + unit_name
 		else
-			return amount.to_s + ' ' + (unit_name).pluralize(amount)
+			return rounded_amount_string + ' ' + (unit_short_name != nil ? unit_short_name : unit_name).pluralize(amount)
 		end
 	end
 end
