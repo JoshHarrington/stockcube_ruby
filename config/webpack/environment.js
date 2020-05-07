69dd53b89a9ac92639e9259d7ecf4091e42ee403
Josh Harrington <josh@joshharrington.co.uk>
2020-05-07 08:41:15 +0100

    BUGGY -- tailwind added but blank page shown

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
 
 const file = {
   test: /\.(jpg|jpeg|png|gif|tiff|ico|svg|eot|otf|ttf|woff|woff2|mp4|mov|webm)$/i,
