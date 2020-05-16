e3e09565efd8e684f8c6b0ff96e6dcd6512377aa
Josh Harrington <josh@joshharrington.co.uk>
2020-05-16 12:12:56 +0100

    Replace feedback form classes with tailwind

--- app/webpacker/functions/feedback.js
+++ app/webpacker/functions/feedback.js
@@ -1,15 +1,15 @@
 import { ready } from "./utils";
 
 const scrollToForm = () => {
-	document.querySelector('.feedback_form_label').addEventListener('click', () => {
-		document.querySelector('.footer_feedback_form--wrapper').scrollIntoView({
+	document.querySelector('[data-name="feedback-form-label"]').addEventListener('click', () => {
+		document.querySelector('[data-name="feedback-form-wrapper"]').scrollIntoView({
 			behavior: 'smooth'
 		});
 	})
 }
 
 const feedback = () => {
-	if (document.querySelector('.footer_feedback_form--wrapper')) {
+	if (document.querySelector('[data-name="feedback-form-wrapper"]')) {
 		scrollToForm()
 	}
 }
