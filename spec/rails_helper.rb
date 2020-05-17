a3e739035f2e88fedb09c045a277f90e7c6456dc
Josh Harrington <josh@joshharrington.co.uk>
2020-05-17 17:48:34 +0100

    Setup static pages controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,9 +8,12 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
 require 'capybara/rspec'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
+require_relative 'support/controller_macros'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -40,6 +43,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, :type => :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -94,3 +98,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+
+
+##### To add inside `describe SomeController do` block
+##### - this signs a user in
+
+# before { sign_in(user) }
+# let!(:user) { create(:user) }
