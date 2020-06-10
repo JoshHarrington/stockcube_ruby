95f1a6a3734b4dddda374508065a2d8a7608108d
Josh Harrington <josh@joshharrington.co.uk>
2020-06-10 18:34:34 +0100

    Start implementing add and removing from planner

--- app/webpacker/components/TooltipWrapper.js
+++ app/webpacker/components/TooltipWrapper.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react"
 import * as classNames from "classnames"
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-const TooltipWrapper = ({text, className, width, children}) => {
+const TooltipWrapper = ({text, className, width, position, children}) => {
 	const [inHover, setHover] = useState(false)
 
 	const widthVar = !!(!!width && typeof width === "number") ? width : 24
@@ -16,9 +16,9 @@ const TooltipWrapper = ({text, className, width, children}) => {
 		>
 			{children}
 			<div className={classNames("absolute bg-black text-white text-xs rounded p-3 right-0 bottom-full text-center", {"hidden": !inHover}, {[widthString]: !!widthVar})}
-			style={{left: "50%", bottom: "calc(100% + .5rem"}}>
+			style={{left: "50%", bottom: !!(!position || position === "top") && "calc(100% + .5rem", top: !!(!!position && position === "bottom") && "calc(100% + .5rem" }}>
 				{text}
-				<svg className="absolute text-black h-2 w-full left-0 bottom-0 -mb-2" x="0px" y="0px" viewBox="0 0 255 255" xmlSpace="preserve"><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
+				<svg className={classNames("absolute text-black h-2 w-full left-0", {"top-0 -mt-2 transform rotate-180": !!position && position === "bottom"}, {"bottom-0 -mb-2": !position || position === "top"})} x="0px" y="0px" viewBox="0 0 255 255" xmlSpace="preserve"><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
 			</div>
 		</div>
 
@@ -26,6 +26,7 @@ const TooltipWrapper = ({text, className, width, children}) => {
 }
 
 TooltipWrapper.propTypes = {
+	position: PropTypes.oneOf(['top', 'bottom']),
 	text: PropTypes.string,
 	width: PropTypes.oneOf([24, 32, 40, 48, 64]),
 	className: PropTypes.string
