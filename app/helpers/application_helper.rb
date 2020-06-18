5a488466fef2368cc32856f340117b05cce6b74b
Josh Harrington <josh@joshharrington.co.uk>
2020-06-18 12:43:34 +0100

    Link to shopping list share page in nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -126,7 +126,7 @@ module ApplicationHelper
 		section = request.path_parameters[:controller]
 		if current_page?(page_path) && !request.parameters.has_key?(:search)
 			return '<li class="current_page"><span class="fake_link">'.html_safe + link_string.html_safe + '</span></li>'.html_safe
-		elsif page_path.to_s.include?(section) || (optional_section != nil ? section.to_s.include?(optional_section) : false)
+		elsif section != "planner" && page_path.to_s.include?(section) || (optional_section != nil ? section.to_s.include?(optional_section) : false)
 			return '<li class="current_section">'.html_safe + link_to(link_string, page_path) + '</li>'.html_safe
 		else
 			return '<li>'.html_safe + link_to(link_string, page_path) + '</li>'.html_safe
