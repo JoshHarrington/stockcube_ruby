5de49bfdf84e88702ac1a2584a44cb10e4b4f523
Josh Harrington <josh@joshharrington.co.uk>
2020-07-15 16:12:21 +0100

    Remove unnecessary extra rspec config

--- spec/controllers/planner_spec.rb
+++ spec/controllers/planner_spec.rb
@@ -9,24 +9,21 @@ describe PlannerController do
 
 end
 
-RSpec.configure do |config|
-  config.before(:example, exceptions: :catch) do
-    allow(Rails.application.config.action_dispatch).to receive(:show_exceptions) { true }
-  end
-end
-
 describe PlannerController do
 
   let!(:user) { create(:user) }
   before do
     sign_in(user)
-    allow_any_instance_of(PlannerController).to receive(:current_user) { user }
   end
 
   it "should get planner" do
     get :index
     expect(response).to be_success
   end
+
+  let(:headers) {{
+    "ACCEPT": "application/json"
+  }}
 
   let!(:unit) { create(:unit)}
   let!(:ingredient) { create(:ingredient)}
@@ -40,14 +37,11 @@ describe PlannerController do
     )
   }
 
-  context "with valid parameters", exceptions: :catch do
+  context "with valid parameters" do
     let(:valid_recipe_id) {recipe.id}
     let(:recipe_id_hash){Hashids.new(ENV['RECIPE_ID_SALT'])}
     let(:valid_params){{
       recipe_id: recipe_id_hash.encode(valid_recipe_id)
-    }}
-    let(:headers) {{
-      "ACCEPT": "application/json"
     }}
 
     it "should let user add recipe to planner" do
@@ -60,18 +54,16 @@ describe PlannerController do
     end
   end
 
-  context "with invalid parameters", exceptions: :catch do
+  context "with invalid parameters" do
     let(:invalid_recipe_id) {recipe.id.to_i + 1}
     let(:invalid_params) {{
       recipe_id: invalid_recipe_id
-    }}
-    let(:headers) {{
-      "ACCEPT": "application/json"
     }}
 
     it "should not let user add recipe to planner" do
       request.headers.merge! headers
       post :recipe_add_to_planner, params: invalid_params
+
       expect(response.content_type).to eq("application/json")
       expect(response).to have_http_status(:not_found)
       expect(PlannerRecipe.where(recipe_id: recipe.id.to_i + 1).length).to eq 0
