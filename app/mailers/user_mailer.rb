c4333b1219a89b9bffb761918cda7555b009b968
Josh Harrington <josh@joshharrington.co.uk>
2020-07-23 17:43:53 +0100

    Add notification if new ingredient is added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,17 @@ class UserMailer < ApplicationMailer
 		)
 	end
 
+	def admin_ingredient_add_notification(user, ingredient)
+		return unless User.where(admin: true).length > 0
+		@user = user
+		@ingredient = ingredient
+		mail(
+			to: User.where(admin: true).first.email,
+			cc: User.where(admin:true).length > 1 ? User.where(admin:true)[1..-1].map(&:email) : nil,
+			subject: "User feedback form submitted"
+		)
+	end
+
+
+
 end
