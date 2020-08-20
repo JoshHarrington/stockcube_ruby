39022280118ce710073101eb6f4f81a6097b3223
Josh Harrington <josh@joshharrington.co.uk>
2020-08-20 10:14:42 +0100

    Make recipes index public Start refactoring recipe routes

--- spec/controllers/recipe_spec.rb
+++ spec/controllers/recipe_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 describe RecipesController do
 
+  context "with no logged in user" do
+    it "should show recipe index" do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should redirect for creating new recipe" do
+      get :new
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
   context "with recipe steps added" do
 
     let!(:user) { create(:user) }
@@ -58,6 +70,48 @@ describe RecipesController do
           expect(step.content).to eq "New Step 2"
         end
       end
+    end
+  end
+
+  context "with logged in user" do
+    let!(:user) { create(:user) }
+    before do
+      user.confirm
+      sign_in(user)
+    end
+
+    let!(:recipe) { create(:recipe) }
+
+    it "should be able to favourite recipe" do
+      get :favourite, params: {
+        id: user.id,
+        type: "favourite"
+      }
+      expect(response).to have_http_status(:redirect)
+      expect(flash[:notice]).to eq "Added the \"<a href=\"#{recipe_path(recipe)}\">#{recipe.title}</a>\" recipe to favourites"
+    end
+
+    it "should be able to unfavourite recipe" do
+      get :favourite, params: {
+        id: user.id,
+        type: "unfavourite"
+      }
+      expect(response).to have_http_status(:redirect)
+      expect(flash[:notice]).to eq "Removed the \"<a href=\"#{recipe_path(recipe)}\">#{recipe.title}</a>\" recipe from favourites"
+    end
+
+  end
+
+  context "with logged in user with json headers" do
+    let(:headers) {{
+      "ACCEPT": "application/json",
+      # "X-CSRF-Token": form_authenticity_token  ## need to provide csrf token
+    }}
+
+    it "should return not allowed for portion delete without portion id" do
+      request.headers.merge! headers
+      post :portion_delete, params: {portion_id: 1}
+      expect(response).to have_http_status(403)
     end
   end
 end
