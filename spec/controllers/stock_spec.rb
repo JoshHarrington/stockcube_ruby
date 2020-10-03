a495156355dcac9fb1008420afafd2205a7b6080
Josh Harrington <josh@joshharrington.co.uk>
2020-10-03 16:04:24 +0100

    Extend stock spec to test update action

--- spec/controllers/stock_spec.rb
+++ spec/controllers/stock_spec.rb
@@ -17,6 +17,8 @@ describe StocksController do
 		end
 
 		let!(:cupboard) { create(:cupboard) }
+		cupboard_id_hashids = Hashids.new(ENV['CUPBOARDS_ID_SALT'])
+		let!(:encoded_cupboard_id) { cupboard_id_hashids.encode(cupboard.id) }
 		let!(:cupboard_user) { create(:cupboard_user, user_id: user.id, cupboard_id: cupboard.id )}
 
 		let!(:unit) { create(:unit) }
@@ -38,7 +40,7 @@ describe StocksController do
 			expect(Stock.exists?(amount: 2, ingredient_id: ingredient.id, unit_id: unit.id, cupboard_id: cupboard.id)).to eq false
 
 			post :create_from_post, params: {
-				cupboardId: cupboard.id,
+				cupboardId: encoded_cupboard_id,
 				ingredient: ingredient.id,
 				unitId: unit.id,
 				amount: 2,
@@ -61,7 +63,7 @@ describe StocksController do
 			)).to eq false
 
 			post :create_from_post, params: {
-				cupboardId: cupboard.id,
+				cupboardId: encoded_cupboard_id,
 				ingredient: "New ingredient",
 				unitId: unit.id,
 				amount: 2,
@@ -78,6 +80,53 @@ describe StocksController do
 				unit_id: unit.id,
 				use_by_date: Date.current + 14.days
 			)).to eq true
+		end
+	end
+
+	context "with logged in user and existing stock" do
+		let!(:user) { create(:user) }
+		before do
+			user.confirm
+			sign_in(user)
+		end
+
+		let!(:cupboard) { create(:cupboard) }
+		cupboard_id_hashids = Hashids.new(ENV['CUPBOARDS_ID_SALT'])
+		let!(:encoded_cupboard_id) { cupboard_id_hashids.encode(cupboard.id) }
+		let!(:cupboard_user) { create(:cupboard_user, user_id: user.id, cupboard_id: cupboard.id )}
+
+		let!(:unit) { create(:unit) }
+		let!(:ingredient) { create(:ingredient) }
+		let!(:stock) {create(:stock, amount: 1, use_by_date: Date.current + 7.days, ingredient_id: ingredient.id, unit_id: unit.id, cupboard_id: cupboard.id)}
+		let!(:stock_user) { create(:stock_user, stock_id: stock.id, user_id: user.id)}
+
+		let(:headers) {{
+      "ACCEPT": "application/json"
+    }}
+
+		it "should return bad request for post without params" do
+			request.headers.merge! headers
+
+			post :update_from_post, params: {}
+			expect(response).to have_http_status(:bad_request)
+		end
+
+		it "should return ok for post with correct params" do
+			request.headers.merge! headers
+
+			expect(Stock.exists?(id: stock.id, amount: 1, ingredient_id: ingredient.id, unit_id: unit.id, cupboard_id: cupboard.id)).to eq true
+
+			post :update_from_post, params: {
+				cupboardId: cupboard.id,
+				stockId: stock.id,
+				unitId: unit.id,
+				amount: 2,
+				useByDate: (Date.current + 14.days).strftime("%F")
+			}
+
+			expect(response.content_type).to eq("application/json")
+			expect(response).to have_http_status(:ok)
+			expect(Stock.exists?(id: stock.id, amount: 2, ingredient_id: ingredient.id, cupboard_id: cupboard.id, unit_id: unit.id, use_by_date: Date.current + 14.days)).to eq true
 		end
 	end
 end
